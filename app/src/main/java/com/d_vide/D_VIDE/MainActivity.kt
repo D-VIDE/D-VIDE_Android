21117fbf03e24f851ce32a89becc269acffd5534
uzun <hyjhyj0901@pusan.ac.kr>
2022-07-13 16:08:03 +0900

    init: project directory init

---
+++ app/src/main/java/com/d_vide/D_VIDE/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.d_vide.D_VIDE
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.d_vide.D_VIDE.ui.theme.DVIDETheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DVIDETheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DVIDETheme {
+        Greeting("Android")
+    }
+}
