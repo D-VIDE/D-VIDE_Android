3c6ed20bd0b86e0d280fa563de9d131aff95236c
WoeunJung <leena09123@naver.com>
2022-07-15 17:07:16 +0900

    design: add RecruitingView

---
+++ app/src/main/java/com/d_vide/D_VIDE/app/presentation/Recruitings/RecruitingsScreen.kt
@@ -0,0 +1,19 @@
+package com.d_vide.D_VIDE.app.presentation.Recruitings
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.d_vide.D_VIDE.app.presentation.Recruitings.component.RecruitingItem
+import com.d_vide.D_VIDE.ui.theme.DVIDETheme
+
+@Composable
+fun RecruitingsScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DVIDETheme {
+        RecruitingsScreen()
+    }
+}
