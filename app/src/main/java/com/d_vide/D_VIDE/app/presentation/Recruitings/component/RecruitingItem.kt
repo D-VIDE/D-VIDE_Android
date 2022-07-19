2d32729a282f1dd38b469fa723ed82fd8f73a197
WoeunJung <leena09123@naver.com>
2022-07-19 20:41:28 +0900

    design: edit Recruiting View

--- app/src/main/java/com/d_vide/D_VIDE/app/presentation/Recruitings/component/RecruitingItem.kt
+++ app/src/main/java/com/d_vide/D_VIDE/app/presentation/Recruitings/component/RecruitingItem.kt
@@ -1,5 +1,7 @@
 package com.d_vide.D_VIDE.app.presentation.Recruitings.component
 
+import android.widget.ProgressBar
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -7,23 +9,33 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
+import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.material.ProgressIndicatorDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.*
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Vertices
 import androidx.compose.ui.graphics.drawscope.clipPath
+import androidx.compose.ui.graphics.vector.PathNode
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,7 +50,7 @@ fun RecruitingItem() {
             .height(161.dp)
             .background(background)
     ) {
-        Row (
+        Row(
             modifier = Modifier
                 .padding(start = 19.dp)
                 .padding(top = 6.dp)
@@ -61,7 +73,8 @@ fun RecruitingItem() {
                     .padding(start = 7.6.dp)
                     .padding(top = 5.dp)
                     .height(21.dp)
-                    .width(100.dp)
+                    .width(100.dp),
+                overflow = TextOverflow.Ellipsis
             )
             Text(
                 text = "세종시 조치원읍",
@@ -78,13 +91,16 @@ fun RecruitingItem() {
             modifier = Modifier
                 .padding(top = 7.dp)
                 .fillMaxWidth()
+                .shadow(elevation = 2.dp, shape = RoundedCornerShape(26.dp), clip = false)
                 .clickable {
                     // TODO
                 }
+
         ) {
-            Canvas(modifier = Modifier
-                .height(121.dp)
-                .width(350.dp)
+            Canvas(
+                modifier = Modifier
+                    .height(121.dp)
+                    .width(350.dp)
             ) {
 
                 val cornerRadius = CornerRadius(26.dp.toPx(), 26.dp.toPx())
@@ -101,30 +117,42 @@ fun RecruitingItem() {
                     )
                 }
                 drawPath(path, color = White)
-                drawRect(
-                    color = graph_gray,
-                    topLeft = Offset(0f, 0f),
-                    size = Size(10.dp.toPx(), 33f.dp.toPx())
+            }
+
+            val progress = 70f
+            val mProgress = animateFloatAsState(targetValue = progress / 100)
+            Column(
+                modifier = Modifier
+                    .background(Color.LightGray)
+                    .fillMaxHeight()
+                    .width(10.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .weight((if ((1 - mProgress.value) == 0f) 0.0001 else 1 - mProgress.value) as Float)
                 )
-                drawRect(
-                    color = mainYellow,
-                    topLeft = Offset(0f, 33.dp.toPx()),
-                    size = Size(10.dp.toPx(), 88f.dp.toPx())
+                Box(
+                    modifier = Modifier
+                        .weight(mProgress.value)
+                        .fillMaxWidth()
+                        .background(
+                            mainYellow
+                        )
                 )
             }
-            Row (
+            Row(
 
             ) {
                 Image(
                     painterResource(id = R.drawable.food),
                     contentScale = ContentScale.FillBounds,
-                     contentDescription = "food",
-                     modifier = Modifier
-                         .padding(start = 19.dp)
-                         .padding(top = 17.dp)
-                         .size(64.dp, 86.dp)
-                         .clip(RoundedCornerShape(12.dp)),
-                     alignment = Alignment.Center
+                    contentDescription = "food",
+                    modifier = Modifier
+                        .padding(start = 19.dp)
+                        .padding(top = 17.dp)
+                        .size(64.dp, 86.dp)
+                        .clip(RoundedCornerShape(12.dp)),
+                    alignment = Alignment.Center
                 )
                 Column() {
                     Text(
@@ -134,30 +162,33 @@ fun RecruitingItem() {
                         modifier = Modifier
                             .padding(top = 18.dp)
                             .padding(start = 18.dp)
-                            .height(41.dp)
-                            .width(222.dp)
+                            .height(39.dp)
+                            .width(222.dp),
+                        overflow = TextOverflow.Ellipsis
                     )
-                    Row (){
-                        Column(){
+                    Row() {
+                        Column(
+                            modifier = Modifier.padding(top = 2.dp)
+                        ) {
                             Text(
                                 text = "마감시간",
                                 fontSize = 10.sp,
                                 fontWeight = FontWeight.W500,
                                 textAlign = TextAlign.Center,
-                                color =text_gray,
+                                color = text_gray,
                                 modifier = Modifier
                                     .padding(start = 16.dp)
-                                    .height(21.dp)
+                                    .height(13.dp)
                                     .width(100.dp)
                             )
-                            Row(){
+                            Row() {
                                 Text(
                                     text = "오후",
                                     fontSize = 10.sp,
                                     fontWeight = FontWeight.Bold,
                                     color = mainYellow,
                                     modifier = Modifier
-                                        .padding(top = 5.dp)
+                                        .padding(top = 10.dp)
                                         .padding(start = 18.dp)
                                         .height(21.dp)
                                         .width(19.dp)
@@ -169,28 +200,64 @@ fun RecruitingItem() {
                                     fontWeight = FontWeight.Bold,
                                     color = mainOrange,
                                     modifier = Modifier
-                                        .padding(top = 1.dp)
                                         .padding(start = 4.dp)
+                                        .height(26.dp)
+                                        .height(62.dp)
                                 )
 
                             }
                         }
+                        Divider(
+                            color = line_gray,
+                            modifier = Modifier
+                                .height(55.dp)
+                                .width(1.dp)
+                        )
+                        Column(
+                            modifier = Modifier.padding(top = 2.dp)
+                        ) {
+                            Text(
+                                text = "부족한 금액",
+                                fontSize = 10.sp,
+                                fontWeight = FontWeight.W500,
+                                textAlign = TextAlign.Center,
+                                color = text_gray,
+                                modifier = Modifier
+                                    .padding(start = 12.dp)
+                                    .height(13.dp)
+                                    .width(100.dp)
+                            )
+                            Row() {
+                                Text(
+                                    text = "12,000",
+                                    fontSize = 22.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = mainOrange,
+                                    modifier = Modifier
+                                        .padding(start = 26.dp)
+                                        .height(26.dp)
+                                        .height(62.dp)
+                                )
+                                Text(
+                                    text = "원",
+                                    fontSize = 10.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = mainYellow,
+                                    modifier = Modifier
+                                        .padding(top = 10.dp)
+                                        .padding(start = 1.dp)
+                                        .height(21.dp)
+                                        .width(19.dp)
+
+                                )
+
+                            }
+                        }
+
                     }
 
                 }
             }
         }
-
-
-    }
-
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DVIDETheme {
-        RecruitingItem()
     }
 }
