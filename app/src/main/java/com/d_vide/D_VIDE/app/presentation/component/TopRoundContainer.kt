c9ed970ebf6d8b9373b09d12df1f7a7200e388a2
uzun <hyjhyj0901@pusan.ac.kr>
2022-07-25 15:22:15 +0900

    feat: add TopRoundBar
    + material3 dependency 추가

--- app/src/main/java/com/d_vide/D_VIDE/app/presentation/component/TopRoundContainer.kt
+++ app/src/main/java/com/d_vide/D_VIDE/app/presentation/component/TopRoundContainer.kt
@@ -3,10 +3,19 @@ package com.d_vide.D_VIDE.app.presentation.component
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIos
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
@@ -19,6 +28,33 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.d_vide.D_VIDE.ui.theme.mainOrange
+
+@Composable
+fun TopRoundBar(
+    text: String = "TopAppBar",
+    actions: @Composable RowScope.() -> Unit = {},
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(
+            text = text,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.ExtraBold,
+            color = mainOrange,
+        ) },
+        navigationIcon = {
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBackIos,
+                    contentDescription = "뒤로가기"
+                )
+            }
+        },
+        actions = actions,
+        modifier = Modifier
+            .fillMaxWidth()
+            .shadow(elevation = 5.dp, shape = RoundedCornerShape(0.dp, 0.dp, 26.dp, 26.dp), clip = true),
+    )
+}
 
 @Composable
 fun TopRoundTextContainer(
@@ -76,10 +112,11 @@ fun TopRoundContainer(
 @Preview
 @Composable
 fun PreviewTopRoundContainer() {
-    Column() {
+    Column {
         TopRoundContainer{}
         TopRoundContainer(){Text("hello")}
         TopRoundTextContainer("D/VIDE 모집글 작성")
+        TopRoundBar("D/VIDE 모집글 작성")
     }
 
 }
