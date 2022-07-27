d6c380623ef3a170c52b40e19943eafdcaf7a8f4
uzun <hyjhyj0901@pusan.ac.kr>
2022-07-27 12:05:37 +0900

    refactor: FAB 컴포넌트 분리

--- app/src/main/java/com/d_vide/D_VIDE/app/presentation/component/FloatingButton.kt
+++ app/src/main/java/com/d_vide/D_VIDE/app/presentation/component/FloatingButton.kt
@@ -5,15 +5,20 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Message
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
+import com.d_vide.D_VIDE.app.presentation.navigation.Screen
 import com.d_vide.D_VIDE.ui.theme.mainOrange
 
 @Composable
@@ -23,16 +28,24 @@ fun FloatingButton(
     fontSize: TextUnit = 20.sp
 ) {
     ExtendedFloatingActionButton(
-        text = { Text(
-            text = text,
-            fontWeight = FontWeight.ExtraBold,
-            color = Color(0xFFFFFFFF),
-            fontSize = fontSize
-        ) },
+        text = { FABTextContent(text, fontSize) },
         backgroundColor = mainOrange,
         onClick = onClick,
         modifier = Modifier.fillMaxWidth(0.9f),
         shape = MaterialTheme.shapes.large.copy(CornerSize(percent = 100))
+    )
+}
+
+@Composable
+fun FABTextContent(
+    text: String = "",
+    fontSize: TextUnit
+) {
+    Text(
+        text = text,
+        fontWeight = FontWeight.ExtraBold,
+        color = Color(0xFFFFFFFF),
+        fontSize = fontSize
     )
 }
 
