e50ad3bfa583e15e54cd2fe59add403681d71347
GaheeKim <aqswefr@naver.com>
2022-08-03 20:38:59 +0900

    fix: image and card clickable
    modify image and card clickable

--- app/src/main/java/com/d_vide/D_VIDE/app/presentation/component/Card.kt
+++ app/src/main/java/com/d_vide/D_VIDE/app/presentation/component/Card.kt
@@ -1,6 +1,7 @@
 package com.d_vide.D_VIDE.app.presentation.component
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
@@ -19,10 +20,16 @@ fun CardTopRound(
     modifier: Modifier = Modifier,
     shape: RoundedCornerShape = RoundedCornerShape(topStart = 26.dp, topEnd = 26.dp),
     color: Color = gray7,
+    onClick: () -> Unit = {},
+    enabled: Boolean = true,
     content: @Composable () -> Unit
 ){
     Surface(
         modifier = modifier
+            .clickable(
+                onClick = onClick,
+                enabled = enabled
+            )
             .coloredShadow(
                 color = gray5,
                 alpha = 0.15f,
@@ -44,10 +51,16 @@ fun CardEndRound(
     modifier: Modifier = Modifier,
     shape: RoundedCornerShape = RoundedCornerShape(topEnd = 26.dp, bottomEnd = 26.dp),
     color: Color = gray7,
+    onClick: () -> Unit = {},
+    enabled: Boolean = true,
     content: @Composable () -> Unit
 ){
     Surface(
         modifier = modifier
+            .clickable(
+                onClick = onClick,
+                enabled = enabled
+            )
             .fillMaxWidth()
             .height(146.dp),
         color = color,
@@ -62,10 +75,16 @@ fun CardRound(
     modifier: Modifier = Modifier,
     shape: RoundedCornerShape = RoundedCornerShape(26.dp),
     color: Color = gray7,
+    onClick: () -> Unit = {},
+    enabled: Boolean = true,
     content: @Composable () -> Unit
 ){
     Surface(
         modifier = modifier
+            .clickable(
+                onClick = onClick,
+                enabled = enabled
+            )
             .fillMaxWidth()
             .height(52.dp),
         color = color,
@@ -80,10 +99,16 @@ fun CardRoundSmall(
     modifier: Modifier = Modifier,
     shape: RoundedCornerShape = RoundedCornerShape(13.dp),
     color: Color = gray7,
+    onClick: () -> Unit = {},
+    enabled: Boolean = true,
     content: @Composable () -> Unit
 ){
     Surface(
         modifier = modifier
+            .clickable(
+                onClick = onClick,
+                enabled = enabled
+            )
             .size(106.dp, 137.dp),
         color = color,
         shape = shape,
