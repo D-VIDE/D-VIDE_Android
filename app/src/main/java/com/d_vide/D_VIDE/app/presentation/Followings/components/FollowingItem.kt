e40b1d06a76c7162fd8162fd8c8c04efa6360b61
WoeunJung <leena09123@naver.com>
2022-08-04 19:59:33 +0900

    design: image에 CircleShape 추가

--- app/src/main/java/com/d_vide/D_VIDE/app/presentation/Followings/components/FollowingItem.kt
+++ app/src/main/java/com/d_vide/D_VIDE/app/presentation/Followings/components/FollowingItem.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterEnd
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -64,6 +65,7 @@ fun FollowerProfileImage(
         modifier = modifier
             .padding(horizontal = 10.dp, vertical = 8.dp)
             .size(50.dp)
+            .clip(CircleShape)
             .border(
                 width = 1.dp,
                 color = mainYellow,
