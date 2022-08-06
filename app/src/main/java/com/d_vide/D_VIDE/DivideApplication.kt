fedebf2e5f08694a7507a631bad58957e67f49e8
uzun <hyjhyj0901@pusan.ac.kr>
2022-08-06 16:27:17 +0900

    feat: di 를 위한 application name 등록
    #33

---
+++ app/src/main/java/com/d_vide/D_VIDE/DivideApplication.kt
@@ -0,0 +1,5 @@
+package com.d_vide.D_VIDE
+
+import android.app.Application
+
+class DivideApplication : Application()
--- app/src/main/java/com/d_vide/D_VIDE/MainActivity.kt
+++ app/src/main/java/com/d_vide/D_VIDE/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.d_vide.D_VIDE.app.presentation.navigation.NavGraph
 import com.d_vide.D_VIDE.ui.theme.DVIDETheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
