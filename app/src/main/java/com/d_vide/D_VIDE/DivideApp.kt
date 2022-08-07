4c415a946735ff987a8b919bdd49edf7359f032d
uzun <hyjhyj0901@pusan.ac.kr>
2022-08-07 11:31:23 +0900

    feat: hilt추가를 위한 베이스 작업

--- app/src/main/java/com/d_vide/D_VIDE/DivideApp.kt
+++ app/src/main/java/com/d_vide/D_VIDE/DivideApp.kt
@@ -1,13 +1,8 @@
 package com.d_vide.D_VIDE
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
 import com.d_vide.D_VIDE.app.presentation.component.BottomNavigationBar
 import com.d_vide.D_VIDE.app.presentation.component.DivideScaffold
-import com.d_vide.D_VIDE.app.presentation.component.FloatingButton
 import com.d_vide.D_VIDE.app.presentation.navigation.NavGraph
 import com.d_vide.D_VIDE.ui.theme.DVIDETheme
 
--- app/src/main/java/com/d_vide/D_VIDE/DivideApplication.kt
+++ app/src/main/java/com/d_vide/D_VIDE/DivideApplication.kt
@@ -1,5 +1,7 @@
 package com.d_vide.D_VIDE
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class DivideApplication : Application()
