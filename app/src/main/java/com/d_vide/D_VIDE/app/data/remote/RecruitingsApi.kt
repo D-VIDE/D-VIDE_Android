bcc5f19aad5fd98e841327f399be4592a1043fdb
uzun <hyjhyj0901@pusan.ac.kr>
2022-08-09 20:31:57 +0900

    feat: 모집글 post api 및 repository 작성
    #40

--- app/src/main/java/com/d_vide/D_VIDE/app/data/remote/RecruitingsApi.kt
+++ app/src/main/java/com/d_vide/D_VIDE/app/data/remote/RecruitingsApi.kt
@@ -1,16 +1,28 @@
 package com.d_vide.D_VIDE.app.data.remote
 
+import com.d_vide.D_VIDE.app.data.remote.dto.RecruitingBodyDTO
+import com.d_vide.D_VIDE.app.data.remote.dto.RecruitingIdDTO
 import com.d_vide.D_VIDE.app.data.remote.dto.RecruitingsDTO
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface RecruitingsApi {
 
-    // 근처 500m
+    // 근처 500m 모집글
     @GET("/api/v1/posts/nearby")
     suspend fun getRecruitings(
         @Query("latitude") latitude: Double,
         @Query("longitude") longitude: Double
     ): Response<RecruitingsDTO>
+
+    // 모집글 작성
+    @POST("/api/v1/post")
+    suspend fun postRecruiting(
+        @Query("userId") userId: Int,
+        @Body recruitingBody: RecruitingBodyDTO
+    ): Response<RecruitingIdDTO>
+
 }
--- app/src/main/java/com/d_vide/D_VIDE/app/data/repository/RecruitingRepositoryImpl.kt
+++ app/src/main/java/com/d_vide/D_VIDE/app/data/repository/RecruitingRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.d_vide.D_VIDE.app.data.repository
 
 import com.d_vide.D_VIDE.app.data.remote.RecruitingsApi
+import com.d_vide.D_VIDE.app.data.remote.dto.RecruitingBodyDTO
+import com.d_vide.D_VIDE.app.data.remote.dto.RecruitingIdDTO
 import com.d_vide.D_VIDE.app.data.remote.dto.RecruitingsDTO
 import com.d_vide.D_VIDE.app.domain.repository.RecruitingRepository
 import retrofit2.Response
@@ -12,5 +14,9 @@ class RecruitingRepositoryImpl @Inject constructor(
 
     override suspend fun getRecruitings(latitude: Double, longitude: Double): Response<RecruitingsDTO> {
         return api.getRecruitings(latitude, longitude)
+    }
+
+    override suspend fun postRecruiting(userId: Int, recruitingBody: RecruitingBodyDTO): Response<RecruitingIdDTO> {
+        return api.postRecruiting(userId, recruitingBody)
     }
 }
