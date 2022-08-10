2d470e89183c6241d92ab2144aa370202f736150
uzun <hyjhyj0901@pusan.ac.kr>
2022-08-10 20:04:58 +0900

    이것저것 수정 (죄송)

--- app/src/main/java/com/d_vide/D_VIDE/app/_constants/RemoteConst.kt
+++ app/src/main/java/com/d_vide/D_VIDE/app/_constants/RemoteConst.kt
@@ -2,6 +2,6 @@ package com.d_vide.D_VIDE.app._constants
 
 class RemoteConst {
     object RemoteConstant {
-        const val BASE_URL = "http://divide.kro.kr:8080/"
+        const val BASE_URL = "http://divide.kro.kr/"
     }
 }
--- app/src/main/java/com/d_vide/D_VIDE/app/data/remote/dto/RecruitingBodyDTO.kt
+++ app/src/main/java/com/d_vide/D_VIDE/app/data/remote/dto/RecruitingBodyDTO.kt
@@ -3,14 +3,14 @@ package com.d_vide.D_VIDE.app.data.remote.dto
 import java.time.LocalDateTime
 
 data class RecruitingBodyDTO(
-    val category: String,
-    val content: String,
-    val deliveryPrice: Int,
-    val latitude: Double,
-    val longitude: Double,
-    val postStatus: String,
-    val storeName: String,
-    val targetPrice: Int,
-    val targetTime: LocalDateTime,
-    val title: String
+    val category: String? = null,
+    val content: String? = null,
+    val deliveryPrice: Int? = null,
+    val latitude: Double? = null,
+    val longitude: Double? = null,
+    val postStatus: String? = null,
+    val storeName: String? = null,
+    val targetPrice: Int? = null,
+    val targetTime: LocalDateTime? = null,
+    val title: String? = null
 )
