3c90ed43d9da131273f7dadfc97a51ac729086cd
uzun <hyjhyj0901@pusan.ac.kr>
2022-08-14 15:11:17 +0900

    feat: 유저 repository 구현
    #46

--- app/src/main/java/com/d_vide/D_VIDE/app/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/d_vide/D_VIDE/app/data/repository/UserRepositoryImpl.kt
@@ -1,31 +1,43 @@
 package com.d_vide.D_VIDE.app.data.repository
 
-import com.d_vide.D_VIDE.app.data.storage.UserInfoStore
-import com.d_vide.D_VIDE.app.domain.model.UserInfo
+import com.d_vide.D_VIDE.app.data.remote.UserApi
+import com.d_vide.D_VIDE.app.data.remote.dto.EmailPasswordDTO
+import com.d_vide.D_VIDE.app.data.remote.dto.UserDTO
+import com.d_vide.D_VIDE.app.data.storage.TokenStore
+import com.d_vide.D_VIDE.app.domain.model.Token
 import com.d_vide.D_VIDE.app.domain.repository.UserRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import retrofit2.Response
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class UserRepositoryImpl @Inject constructor(
-    private val store: UserInfoStore
+    private val store: TokenStore,
+    private val api: UserApi
 ) : UserRepository {
     private val scope = CoroutineScope(Dispatchers.IO)
+    override suspend fun doLogin(emailPw: EmailPasswordDTO): Response<Token> {
+        return api.login(emailPw)
+    }
 
-    override fun getLoggedInUser() {
+    override suspend fun getUserInfo(): Response<UserDTO> {
+        return api.getUserInfo()
+    }
+
+    override fun getUserToken(): Token {
         return runBlocking {
-            store.getLoggedInUser().first()
+            store.getToken().first()
         }
     }
 
-    override fun setLoggedInUser(user: UserInfo) {
+    override fun setUserToken(token: Token) {
         scope.launch {
-            store.setLoggedInUser(user)
+            store.setToken(token)
         }
     }
 }
--- app/src/main/java/com/d_vide/D_VIDE/app/domain/repository/UserRepository.kt
+++ app/src/main/java/com/d_vide/D_VIDE/app/domain/repository/UserRepository.kt
@@ -1,10 +1,15 @@
 package com.d_vide.D_VIDE.app.domain.repository
 
-import com.d_vide.D_VIDE.app.domain.model.UserInfo
+import com.d_vide.D_VIDE.app.data.remote.dto.EmailPasswordDTO
+import com.d_vide.D_VIDE.app.data.remote.dto.UserDTO
+import com.d_vide.D_VIDE.app.domain.model.Token
+import retrofit2.Response
 
 interface UserRepository {
 
-    fun getLoggedInUser(): UserInfo
+    suspend fun doLogin(emailPw: EmailPasswordDTO): Response<Token>
+    suspend fun getUserInfo(): Response<UserDTO>
 
-    fun setLoggedInUser(user: UserInfo)
+    fun getUserToken(): Token
+    fun setUserToken(token: Token)
 }
