ac53767e77c659b34f618fa8a7cc9bcc708cbd70
WoeunJung <leena09123@naver.com>
2022-08-14 20:01:51 +0900

    feat: make RecruitingDetail BottomSheet, scrollable(not LazyColumn)

--- app/src/main/java/com/d_vide/D_VIDE/app/presentation/RecruitingDetail/RecruitingDetail.kt
+++ app/src/main/java/com/d_vide/D_VIDE/app/presentation/RecruitingDetail/RecruitingDetail.kt
@@ -1,110 +1,156 @@
 package com.d_vide.D_VIDE.app.presentation.RecruitingDetail
 
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.LinearProgressIndicator
-import androidx.compose.material.Text
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.BottomCenter
-import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterEnd
 import androidx.compose.ui.Alignment.Companion.CenterStart
-import androidx.compose.ui.Alignment.Companion.End
-import androidx.compose.ui.Alignment.Companion.Start
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.lerp
-import androidx.compose.ui.zIndex
-import com.d_vide.D_VIDE.app.presentation.PostRecruiting.component.EditableFieldItem
-import com.d_vide.D_VIDE.app.presentation.Recruitings.component.LocationSelector
+import androidx.navigation.NavController
 import com.d_vide.D_VIDE.app.presentation.component.DivideButton
 import com.d_vide.D_VIDE.app.presentation.component.DivideDivider
 import com.d_vide.D_VIDE.app.presentation.component.DivideImage
 import com.d_vide.D_VIDE.app.presentation.component.TopRectangleBar
 import com.d_vide.D_VIDE.ui.theme.*
 import com.google.accompanist.pager.*
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import kotlin.math.absoluteValue
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun RecruitingDetail(
-    postId: Int,
-    upPress: () -> Unit
+    postId: Int = 0,
+    navController: NavController,
+    upPress: () -> Unit = {}
 ){
-    Box(modifier = Modifier.fillMaxSize()) {
-        LazyColumn() {
-            item {
-                TopRectangleBar(
-                    title = "룡룡",
-                    imageURL = "https://item.kakaocdn.net/do/6905d2b5d05b4d0b27ec8731cb8252fa7f9f127ae3ca5dc7f0f6349aebcdb3c4",
-                    upPress = upPress
-                )
-                Spacer(modifier = Modifier.size(15.dp))
-                RecrutingPager()
-                Spacer(modifier = Modifier.size(34.dp))
-                Column(modifier = Modifier.padding(horizontal = 36.dp)) {
-                    Text(text = "삼첩분식 드실 분~", style = TextStyles.Big1, color = gray5)
-                    DivideDivider(
-                        Modifier
-                            .alpha(0.2f)
-                            .padding(vertical = 14.dp))
-                    RecruitingSmallText(text = "마감시간", subtext = "04:00", unit ="PM")
-                    Spacer(modifier = Modifier.size(12.dp))
-                    RecruitingSmallText(text = "배달비", subtext = "30,000", unit ="원")
-                    DivideDivider(
-                        Modifier
-                            .alpha(0.2f)
-                            .padding(vertical = 14.dp))
-                    RecruitingBigText(text = "목표 주문 금액", subtext = "30,000", unit ="원")
-                    Spacer(modifier = Modifier.size(12.dp))
-                    RecruitingBigText(text = "현재 주문 금액", subtext = "16,000", unit ="원")
+    val scrollableState = rememberScrollState()
 
-                    LinearProgressIndicator(
-                        progress = 0.7f,
-                        color = main0,
-                        backgroundColor = gray4,
-                        modifier = Modifier
-                            .padding(vertical = 19.dp)
-                            .fillMaxWidth()
-                            .height(16.dp)
-                            .clip(RoundedCornerShape(50.dp))
+    BottomSheetOrderForm(
+        navController = navController
+    ) { state, scope ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(scrollableState)
+        ) {
+            Column{
+                    TopRectangleBar(
+                        title = "룡룡",
+                        imageURL = "https://item.kakaocdn.net/do/6905d2b5d05b4d0b27ec8731cb8252fa7f9f127ae3ca5dc7f0f6349aebcdb3c4",
+                        upPress = upPress
                     )
+                    Spacer(modifier = Modifier.size(15.dp))
+                    RecrutingPager()
+                    Spacer(modifier = Modifier.size(34.dp))
+                    Column(modifier = Modifier.padding(horizontal = 36.dp)) {
+                        Text(text = "삼첩분식 드실 분~", style = TextStyles.Big1, color = gray5)
+                        DivideDivider(
+                            Modifier
+                                .alpha(0.2f)
+                                .padding(vertical = 14.dp)
+                        )
+                        RecruitingSmallText(text = "마감시간", subtext = "04:00", unit = "PM")
+                        Spacer(modifier = Modifier.size(12.dp))
+                        RecruitingSmallText(text = "배달비", subtext = "30,000", unit = "원")
+                        DivideDivider(
+                            Modifier
+                                .alpha(0.2f)
+                                .padding(vertical = 14.dp)
+                        )
+                        RecruitingBigText(text = "목표 주문 금액", subtext = "30,000", unit = "원")
+                        Spacer(modifier = Modifier.size(12.dp))
+                        RecruitingBigText(text = "현재 주문 금액", subtext = "16,000", unit = "원")
 
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        verticalAlignment = Alignment.Bottom,
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Text(text = "• 장소", style = TextStyles.Point4, color = gray3)
-                        Text(text = "역삼디오빌 앞", style = TextStyles.Point4, color = gray3, textAlign = TextAlign.End)
-                    }
+                        LinearProgressIndicator(
+                            progress = 0.7f,
+                            color = main0,
+                            backgroundColor = gray4,
+                            modifier = Modifier
+                                .padding(vertical = 19.dp)
+                                .fillMaxWidth()
+                                .height(16.dp)
+                                .clip(RoundedCornerShape(50.dp))
+                        )
 
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.Bottom,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Text(text = "• 장소", style = TextStyles.Point4, color = gray3)
+                            Text(
+                                text = "역삼디오빌 앞",
+                                style = TextStyles.Point4,
+                                color = gray3,
+                                textAlign = TextAlign.End
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(180.dp))
                 }
             }
-
-        }
-
-        DivideButton(
-            onClick = {},
-            modifier = Modifier
-                .padding(bottom = 30.dp, start = 20.dp, end = 20.dp)
-                .align(Alignment.BottomCenter)
-                .height(50.dp)
-                .fillMaxWidth()) {
-            Text(text = "지금 D/VIDE하기", style = TextStyles.Big1, color = gray7)
+            DivideButton(
+                onClick = {
+                    scope.launch {
+                        state.animateTo(
+                            ModalBottomSheetValue.Expanded,
+                            tween(500)
+                        )
+                    }
+                },
+                modifier = Modifier
+                    .padding(bottom = 30.dp, start = 20.dp, end = 20.dp)
+                    .height(50.dp)
+                    .align(BottomCenter)
+                    .fillMaxWidth()
+            ) {
+                Text(text = "지금 D/VIDE하기", style = TextStyles.Big1, color = gray7)
+            }
         }
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun BottomSheetOrderForm(
+    navController: NavController,
+    activityContentScope: @Composable (state: ModalBottomSheetState, scope: CoroutineScope) -> Unit,
+){
+    val state = rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden,
+        skipHalfExpanded = true
+    )
+    val scope = rememberCoroutineScope()
+
+    ModalBottomSheetLayout(
+        sheetElevation = 5.dp,
+        sheetShape = RoundedCornerShape(topStart = 36.dp, topEnd = 36.dp),
+        sheetState = state,
+        sheetContent = {
+            RecruitingOrderForm(
+                modifier = Modifier.fillMaxHeight(0.7f),
+
+            )
+        }
+    ) {
+        activityContentScope(state, scope)
+    }
+}
 
 @Composable
 fun RecruitingSmallText(
