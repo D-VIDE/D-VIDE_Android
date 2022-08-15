f9a7d6f9698be38b82209043ec98c07bce519184
uzun <hyjhyj0901@pusan.ac.kr>
2022-08-15 14:33:29 +0900

    feat: Uri to File Helper 구현
    #61

---
+++ app/src/main/java/com/d_vide/D_VIDE/app/domain/util/UriFileHelper.kt
@@ -0,0 +1,50 @@
+package com.d_vide.D_VIDE.app.domain.util
+
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
+import java.io.File
+import java.io.FileOutputStream
+
+object FileUtil {
+    // 임시 파일 생성
+    fun createTempFile(context: Context, fileName: String): File {
+        val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File(storageDir, fileName)
+    }
+
+    // 파일 내용 스트림 복사
+    fun copyToFile(context: Context, uri: Uri, file: File) {
+        val inputStream = context.contentResolver.openInputStream(uri)
+        val outputStream = FileOutputStream(file)
+
+        val buffer = ByteArray(4 * 1024)
+        while (true) {
+            val byteCount = inputStream!!.read(buffer)
+            if (byteCount < 0) break
+            outputStream.write(buffer, 0, byteCount)
+        }
+
+        outputStream.flush()
+    }
+}
+
+object UriUtil {
+    // URI -> File
+    fun toFile(context: Context, uri: Uri): File {
+        val fileName = getFileName(context, uri)
+
+        val file = FileUtil.createTempFile(context, fileName)
+        FileUtil.copyToFile(context, uri, file)
+
+        return File(file.absolutePath)
+    }
+
+    // get file name & extension
+    fun getFileName(context: Context, uri: Uri): String {
+        val name = uri.toString().split("/").last()
+        val ext = context.contentResolver.getType(uri)!!.split("/").last()
+
+        return "$name.$ext"
+    }
+}
