35c244b8688a5ddbcc4a8bae0830bf261be2291a
WoeunJung <leena09123@naver.com>
2022-08-15 10:55:45 +0900

    feat: add PostRecruitingOrderViewModel and event

--- app/src/main/java/com/d_vide/D_VIDE/app/presentation/RecruitingDetail/PostRecruitingOrderForm.kt
+++ app/src/main/java/com/d_vide/D_VIDE/app/presentation/RecruitingDetail/PostRecruitingOrderForm.kt
@@ -1,5 +1,6 @@
 package com.d_vide.D_VIDE.app.presentation.RecruitingDetail
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
@@ -9,29 +10,50 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Bottom
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import com.d_vide.D_VIDE.R
+import com.d_vide.D_VIDE.app.presentation.PostRecruiting.PostRecruitingsEvent
 import com.d_vide.D_VIDE.app.presentation.RecruitingDetail.component.OrderFormField
 import com.d_vide.D_VIDE.app.presentation.Recruitings.component.PhotoPicker
 import com.d_vide.D_VIDE.app.presentation.component.DivideButton
+import com.d_vide.D_VIDE.app.presentation.navigation.Screen
+import com.d_vide.D_VIDE.app.presentation.util.addFocusCleaner
 import com.d_vide.D_VIDE.ui.theme.TextStyles
 import com.d_vide.D_VIDE.ui.theme.gray7
 import com.d_vide.D_VIDE.ui.theme.main1
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun RecruitingOrderForm(
+    navController: NavController,
+    postId: Int = 0,
+    viewModel: PostRecruitingOrderViewModel = hiltViewModel(),
     modifier: Modifier = Modifier
 ){
     val scrollableState = rememberScrollState()
+    LaunchedEffect(key1 = true) {
+        viewModel.eventFlow.collectLatest { event ->
+            when(event) {
+                is PostRecruitingOrderViewModel.UiEvent.ShowSnackbar -> {
+                    Log.d("test", "뭔가 문제")
+                    navController.navigateUp()
+                }
+                is PostRecruitingOrderViewModel.UiEvent.SaveRecruiting -> {
+                    Log.d("test", "성공 & 뒤로가자")
+                }
+            }
+        }
+    }
     Column(
         modifier = modifier
             .clip(RoundedCornerShape(topStart = 26.dp, topEnd = 26.dp))
@@ -39,6 +61,7 @@ fun RecruitingOrderForm(
             .padding(horizontal = 20.dp)
             .fillMaxWidth()
             .verticalScroll(scrollableState)
+            .addFocusCleaner(LocalFocusManager.current)
     ){
         Column(
             modifier = Modifier.padding(horizontal = 15.dp)
@@ -77,7 +100,16 @@ fun RecruitingOrderForm(
                         .padding(end = 34.dp)
                 )
                 OrderFormField(
-                    unitText = "원"
+                    unitText = "원",
+                    singleLine = true,
+                    inputText = viewModel.recruitingOrderDTO.value.orderPrice?.toString() ?: "",
+                    onValueChange = {
+                        viewModel.onEvent(
+                            PostRecruitingOrderEvent.EnteredOrderPrice(
+                                if (it.isNullOrBlank()) null else it.toInt()
+                            )
+                        )
+                    }
                 )
             }
             Spacer(modifier = Modifier.height(59.dp))
@@ -85,7 +117,10 @@ fun RecruitingOrderForm(
         }
         DivideButton(
             onClick = {
+                viewModel.onEvent(PostRecruitingOrderEvent.EnteredPostId(postId.toLong()))
+                viewModel.onEvent(PostRecruitingOrderEvent.SaveRecruitingOrder)
 
+                navController.navigateUp()
             },
             modifier = Modifier
                 .padding(bottom = 50.dp)
@@ -95,10 +130,4 @@ fun RecruitingOrderForm(
             Text(text = "D/VIDE!", style = TextStyles.Big1, color = gray7)
         }
     }
-}
-
-@Preview
-@Composable
-fun PreviewRecruitingOrderForm() {
-    RecruitingOrderForm(Modifier)
 }
