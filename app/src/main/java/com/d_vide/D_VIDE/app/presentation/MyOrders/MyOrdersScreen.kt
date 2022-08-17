9d007816373e5c7da4e609e72b7f94ce06877444
WoeunJung <leena09123@naver.com>
2022-08-17 14:12:02 +0900

    feat: add MyCompletedOrder design

--- app/src/main/java/com/d_vide/D_VIDE/app/presentation/MyOrders/MyOrdersScreen.kt
+++ app/src/main/java/com/d_vide/D_VIDE/app/presentation/MyOrders/MyOrdersScreen.kt
@@ -2,29 +2,21 @@
 
 package com.d_vide.D_VIDE.app.presentation.MyOrders
 
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.d_vide.D_VIDE.app.presentation.Recruitings.BlankIndicator
-import com.d_vide.D_VIDE.app.presentation.Recruitings.component.RecruitingItem
+import com.d_vide.D_VIDE.app.presentation.MyOrders.component.MyCompletedOrder
 import com.d_vide.D_VIDE.app.presentation.UserFeed.BottomSheetUserFeedSreen
 import com.d_vide.D_VIDE.app.presentation.component.TopRoundTextContainer
-import com.d_vide.D_VIDE.app.presentation.util.GradientCompponent
-import com.d_vide.D_VIDE.app.presentation.util.LocationConverter
-import com.d_vide.D_VIDE.app.presentation.util.convertTimestampToHour
-import com.d_vide.D_VIDE.app.presentation.util.convertTimestampToMinute
+import com.d_vide.D_VIDE.app.presentation.util.*
 import com.d_vide.D_VIDE.ui.theme.background
-import com.google.android.gms.maps.model.LatLng
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -57,41 +49,20 @@ fun MyOrdersScreen(
 
                     viewModel.state.value.recruitingDTOs.forEach {
                         item {
-                            RecruitingItem(
-                                onUserClick = {
-                                    scope.launch {
-                                        state.animateTo(
-                                            ModalBottomSheetValue.Expanded,
-                                            tween(500)
-                                        )
-                                    }
-                                },
+                            MyCompletedOrder(
                                 onClick = { onRecruitingClick(it.post.id) },
-                                userName = it.user.nickname,
-                                userLocation = LocationConverter(
-                                    LatLng(
-                                        it.post.latitude,
-                                        it.post.longitude
-                                    )
-                                ),
+                                onButtonClick = { },
                                 title = it.post.title,
                                 imageURL = it.user.profileImgUrl,
-                                insufficientMoney =
-                                if (it.post.targetPrice > it.post.orderedPrice)
-                                    it.post.targetPrice - it.post.orderedPrice
-                                else 0,
-                                timeRemaining = ((it.post.targetTime - System.currentTimeMillis() / 1000) / 60),
-                                deadLineHour = it.post.targetTime.convertTimestampToHour(),
-                                deadLineMinute = it.post.targetTime.convertTimestampToMinute()
+                                price = it.post.orderedPrice,
+                                time = it.post.targetTime.convertTimestampToHour()
+                                    .toString() + ":"
+                                        + it.post.targetTime.convertTimestampToMinute()
+                                    .toString(),
+                                date = (it.post.targetTime * 1000).convertTimestampToPointFullDate(),
+                                enabled = if (it.post.status == "RECRUIT_SUCCESS") true else false
                             )
                         }
-                    }
-                    item {
-                        BlankIndicator(
-                            modifier = Modifier
-                                .align(Alignment.CenterHorizontally)
-                                .padding(vertical = 78.dp)
-                        )
                     }
                 }
             }
