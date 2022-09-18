e6a961ddb88357c49099c07707c6d59a377af2fe
GaheeKim <aqswefr@naver.com>
2022-09-18 14:44:10 +0900

    feat: create my review view model and connect
    create my review viewmodel

--- app/src/main/java/com/d_vide/D_VIDE/app/presentation/MyReviews/MyReviewsScreen.kt
+++ app/src/main/java/com/d_vide/D_VIDE/app/presentation/MyReviews/MyReviewsScreen.kt
@@ -6,9 +6,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterEnd
 import androidx.compose.ui.Alignment.Companion.CenterStart
@@ -20,6 +22,7 @@ import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.d_vide.D_VIDE.R
@@ -37,6 +40,9 @@ fun MyReviewsScreen(
     onTagClick: (String) -> Unit,
     upPress: () -> Unit = {}
 ){
+    val viewModel  = hiltViewModel<MyReviewsViewModel>()
+    val reviews  =  viewModel.state.value.reviewsDTO
+
     Column(
         modifier = Modifier
             .background(White)
@@ -49,11 +55,15 @@ fun MyReviewsScreen(
             Image(
                 painter = painterResource(id = R.drawable.up_press_gray),
                 contentDescription = "upPress",
-                modifier = Modifier.size(12.dp, 19.dp).clickable(onClick = upPress)
+                modifier = Modifier
+                    .size(12.dp, 19.dp)
+                    .clickable(onClick = upPress)
             )
             Text(
                 text = "룡룡님 피드",
-                modifier = Modifier.padding(start = 18.dp).align(CenterStart),
+                modifier = Modifier
+                    .padding(start = 18.dp)
+                    .align(CenterStart),
                 style = TextStyles.Basics4
             )
             MoreButton(
@@ -68,10 +78,10 @@ fun MyReviewsScreen(
             contentPadding = PaddingValues(vertical = 28.dp),
             verticalArrangement = Arrangement.spacedBy(15.dp)
         ) {
-            item {
+            items(reviews) { item ->
                 ReviewItem(
-                    onReviewClick = { onReviewSelected(1234) },
-                    onTagClick = { onTagClick("금돼지식당 복천점") },
+                    onReviewClick = { onReviewSelected(item.review.reviewId.toInt()) },
+                    onTagClick = { onTagClick(item.review.storeName) },
                     isliked = false
                 )
             }
