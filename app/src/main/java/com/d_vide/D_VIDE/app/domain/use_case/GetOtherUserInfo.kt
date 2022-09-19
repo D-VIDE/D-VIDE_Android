c1837a8aa6a98466d727c9fc7772ef8634e5af62
WoeunJung <leena09123@naver.com>
2022-09-19 13:44:08 +0900

    feat: other user info domain layer

---
+++ app/src/main/java/com/d_vide/D_VIDE/app/domain/use_case/GetOtherUserInfo.kt
@@ -0,0 +1,34 @@
+package com.d_vide.D_VIDE.app.domain.use_case
+
+import com.d_vide.D_VIDE.app.data.remote.responseDTO.OtherUserInfoDTO
+import com.d_vide.D_VIDE.app.domain.repository.UserRepository
+import com.d_vide.D_VIDE.app.domain.util.Resource
+import com.d_vide.D_VIDE.app.domain.util.log
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetOtherUserInfo @Inject constructor(
+    private val repository: UserRepository,
+) {
+    operator fun invoke(userId: Long): Flow<Resource<OtherUserInfoDTO>> = flow {
+        try {
+            emit(Resource.Loading())
+            val r = repository.getOtherUserInfo(userId)
+            when(r.code()) {
+                200 -> { emit(Resource.Success(r.body()!!)) }
+                else -> {
+                    emit(Resource.Error("getOtherUserInfo failed"))
+                    "getOtherUserInfo usecase ERROR ${r.code()}: ${r.errorBody().toString()}".log()
+                }
+            }
+
+        } catch(e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured"))
+        } catch(e: IOException) {
+            emit(Resource.Error("Couldn't reach server. Check your internet connection."))
+        }
+    }
+}
