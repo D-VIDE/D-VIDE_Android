5ecca4d3cff749b5baaa09475473a0cc90bfb18f
WoeunJung <leena09123@naver.com>
2022-11-13 21:01:40 +0900

    fix: TaggedReviews Api solved in presentation layer

--- app/src/main/java/com/d_vide/D_VIDE/app/presentation/TaggedReviews/TaggedReviewsState.kt
+++ app/src/main/java/com/d_vide/D_VIDE/app/presentation/TaggedReviews/TaggedReviewsState.kt
@@ -1,9 +1,9 @@
 package com.d_vide.D_VIDE.app.presentation.TaggedReviews
 
-import com.d_vide.D_VIDE.app.data.remote.responseDTO.Review.StoreReviewDTO
+import com.d_vide.D_VIDE.app.data.remote.responseDTO.Review.ReviewDTO
 
 data class TaggedReviewsState(
     val isLoading: Boolean = false,
-    val reviews: List<StoreReviewDTO> = emptyList(),
+    val reviews: List<ReviewDTO> = emptyList(),
     val error: String = ""
 )
