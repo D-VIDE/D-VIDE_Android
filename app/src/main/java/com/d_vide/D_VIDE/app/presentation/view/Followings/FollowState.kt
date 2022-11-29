b18af8ac2c93bc62f678a4e3725ce5666b9f5683
GaheeKim <aqswefr@naver.com>
2022-11-29 18:25:11 +0900

    feat: add pagination in follow
    팔로우와 관련된 모든 부분에 pagination 적용

--- app/src/main/java/com/d_vide/D_VIDE/app/presentation/view/Followings/FollowState.kt
+++ app/src/main/java/com/d_vide/D_VIDE/app/presentation/view/Followings/FollowState.kt
@@ -7,5 +7,8 @@ data class FollowState(
     val isLoading: Boolean = false,
     val follows: List<FollowInfoDataDTO> = emptyList(),
     val otherFollows: List<OtherFollowDataDTO> = emptyList(),
-    val error: String = ""
+    val error: String = "",
+    val offset: Int = 0,
+    val endReached: Boolean = false,
+    val pagingLoading : Boolean = false,
 )
